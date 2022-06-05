1fce32336e84f4aa8edccf4207864fa6b2c75e9e
Lu Summers <lusummers@users.noreply.github.com>
2022-06-04 20:42:01 -0400

    added values for product models. fixed seeding issues

--- models/Product.js
+++ models/Product.js
@@ -20,6 +20,18 @@ Product.init(
         type: DataTypes.STRING,
         allowNull: false
       },
+      price:{
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate:{isDecimal: true}, 
+      },
+  
+      
+      stock:{  type: DataTypes.INTEGER,
+        allowNull: false,
+        validate:{isNumeric: true}, 
+        defaultValue:10
+      },
     
       category_id: {
         type: DataTypes.INTEGER,
